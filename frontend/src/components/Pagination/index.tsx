dfdb61fe35678349945bdf8694a5e06c20922fcd
PedroBonorin0 <pepebonorinobraga@gmail.com>
2021-09-10 13:51:18 -0300

    Erro paginacao

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -18,7 +18,7 @@ const Pagination = ( { page, onPageChange } : Props) => {
                         <span className="page-link">{page.number + 1}</span>
                     </li>
                     <li className={`page-item ${page.last ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Próxima</button>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
                     </li>
                 </ul>
             </nav>
